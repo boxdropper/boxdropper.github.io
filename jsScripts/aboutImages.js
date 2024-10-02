50bc9ce4cd40059859f2b91b3955493acff19f3f
Sylenth <ljc.05@outlook.com>
2024-10-02 10:40:28 +0100

    New images for the about page

--- jsScripts/aboutImages.js
+++ jsScripts/aboutImages.js
@@ -6,6 +6,8 @@ var myPix = new Array("images/stills/miku_high_res.jpg", //high-res hatsune miku
 "images/stills/PRB-GTAIV-CharacterDecal.webp", //GTA IV Decal of Princess Robot Bubblegum - Courtesy of GTA Wiki
 "images/stills/PRB-GTAV-UpdatedBattleSuit.webp", //GTA V Updated Design of Princess Robot Bubblegum - Courtesy of GTA Wiki
 "images/stills/PRB-GTAV-ShinyWasabiKitty.webp", //GTA V Shiny Wasabi Kitty - Courtesy of GTA Wiki
+"images/stills/Dreamcast_render.webp", //Sega Hard Girls - Dreamcast US/JP
+"images/stills/Dreamcast_Europe_Version_render.webp", //Sega Hard Girls - Dreamcast PAL
 );
 
 function chooseAboutPic() {
