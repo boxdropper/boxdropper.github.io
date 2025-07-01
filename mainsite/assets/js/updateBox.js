70878918b5c2ebe114936461b7a4360baeb7b302
Sylenth <ljc.05@outlook.com>
2025-07-01 21:19:38 +0100

    Update Box now celebrates the new domain name
    yay for custom domains!

--- mainsite/assets/js/updateBox.js
+++ mainsite/assets/js/updateBox.js
@@ -1,7 +1,7 @@
-let updateTitle = "New Banners Added!"
+let updateTitle = "New Domain Name!"
 document.getElementById("updateTitle").innerHTML = updateTitle;
 
-let data = ["New Raidou Kuzunoha themed banners and logos, Raidou Remaster coming, get hyped.",];
+let data = ["Had I known that it was this easy, I would've done it sooner, but hey, it's here now!",];
         let list = document.getElementById("updateList");
         for (i = 0; i < data.length; ++i) {
             let li = document.createElement('li');
